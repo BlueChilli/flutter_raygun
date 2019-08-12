e42040ed2ddeda57badd4686ef7feb9d52a5dcc7
Max Onishi <max@bluechilli.com>
2019-08-12 14:27:18 +1000

    fix bug

--- ios/Classes/SwiftRaygunPlugin.swift
+++ ios/Classes/SwiftRaygunPlugin.swift
@@ -98,8 +98,7 @@ public class SwiftRaygunPlugin: NSObject, FlutterPlugin, RaygunOnBeforeSendDeleg
   
     private func onInitialisedMethodCall(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         Log(msg: "%@: ApiKey is %@", [Tag, raygunApiKey!])
-        let raygun = Raygun.sharedReporter(withApiKey: raygunApiKey!, withCrashReporting: true) as! Raygun;
-
+        let raygun = Raygun.sharedReporter() as! Raygun;
         switch call.method {
         case "reportCrash":
             let exception = (call.arguments as! Dictionary<String, Any>)
