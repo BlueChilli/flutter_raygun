177382d7e45f1cb069c83a2eba5e5ff1c966b20d
Max Onishi <max@bluechilli.com>
2019-11-25 12:12:14 +1100

    fix swift

--- ios/Classes/RaygunPlugin.m
+++ ios/Classes/RaygunPlugin.m
@@ -1,13 +1,12 @@
 #import "RaygunPlugin.h"
+#if __has_include(<raygun/raygun-Swift.h>)
 #import <raygun/raygun-Swift.h>
-// #if __has_include(<raygun/raygun-Swift.h>)
-
-// #else
-// // Support project import fallback if the generated compatibility header
-// // is not copied when this plugin is created as a library.
-// // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-// #import "raygun-Swift.h"
-// #endif
+#else
+// Support project import fallback if the generated compatibility header
+// is not copied when this plugin is created as a library.
+// https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
+#import "raygun-Swift.h"
+#endif
 
 @implementation RaygunPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
